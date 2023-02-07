1e36f22777b0ef7fe139f79b027ad3ef3ffa8f2e
tanzicai <1728561744@qq.com>
2023-02-07 14:44:12 +0800

    SpringBoot2+H2+Mockito实战 使用Spring Boot  + H2 + Lombok + Spring Boot Test (默认包含了 Junit5 + Mockito)进行单元测试的demo初始化

---
+++ SpringBoot-unitTestDemo-h2/src/main/java/com/tanzicai/springbootunittestdemoh2/SpringBootUnitTestDemoH2Application.java
@@ -0,0 +1,13 @@
+package com.tanzicai.springbootunittestdemoh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUnitTestDemoH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUnitTestDemoH2Application.class, args);
+    }
+
+}
---
+++ SpringBoot-unitTestDemo-h2/src/test/java/com/tanzicai/springbootunittestdemoh2/SpringBootUnitTestDemoH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanzicai.springbootunittestdemoh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUnitTestDemoH2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
